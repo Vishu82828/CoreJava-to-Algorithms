f3619093701fb18a8dfeddc109b8b81a619843c9
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2026-03-31 09:57:16 +0530

    Day 08: Completed BST with all 3 traversals and O(log n) search

--- src/MyFirstTree.java
+++ src/MyFirstTree.java
@@ -1,17 +1,3 @@
-public class MyFirstTree {
-    public static void main(String[] args) {
-        BinarSearchTree tree = new BinarSearchTree();
-        tree.insert(50);
-        tree.insert(30);
-        tree.insert(70);
-        tree.insert(20);
-        tree.insert(40);
-        tree.insert(10);
-        System.out.print("Shorted Tree(In Order) : ");
-        tree.inOrder(tree.root);
-    }
-}
-
 class TreeNode {
     protected int data;
     TreeNode right;
@@ -34,9 +20,29 @@ class BinarSearchTree {
         if(root == null){
             return;
         }
+        //Recursive Steps, go far left as possible
         inOrder(root.left);
         System.out.print(root.data + " ");
+        //Recursive Steps, go far Right as possible
         inOrder(root.right);
+    }
+
+    public void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public void postOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
     }
 
 //    the login to find the right side branch to store data
@@ -52,5 +58,41 @@ class BinarSearchTree {
             root.right = insertRecursive(root.right, data);
         }
         return root;
+    }
+
+    public boolean search(TreeNode root, int key){
+        if(root == null){
+            return false;
+        }
+        if(root.data == key){return true;}
+        if(root.data > key){return search(root.left, key);}
+        else {return search(root.right, key);}
+    }
+}
+
+public class MyFirstTree {
+    public static void main(String[] args) {
+        BinarSearchTree tree = new BinarSearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(10);
+        System.out.print("Shorted Tree(In Order) : ");
+        tree.inOrder(tree.root);
+        System.out.println(" ");
+        System.out.print("Shorted Tree(Pre Order) : ");
+        tree.preOrder(tree.root);
+        System.out.println(" ");
+        System.out.print("Shorted Tree(Post Order) : ");
+        tree.postOrder(tree.root);
+        System.out.println(" ");
+
+        int key = 20;
+        if(tree.search(tree.root, key)){
+            System.out.println(" Key found in Tree ");
+        }
+        else {System.out.println(key + " is not there. ❌");}
     }
 }
